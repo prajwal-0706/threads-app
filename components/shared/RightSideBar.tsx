2a308c300e57f1f8314621fc2c6039fd24117a50
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-11-23 19:56:50 +0530

    Update

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -5,6 +5,8 @@ export default function RightSideBar() {
         <h3 className="text-heading4-medium text-light-1">
           Suggested Communities
         </h3>
+      </div>
+      <div className="flex flex-1 flex-col justify-start ">
         <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
       </div>
     </section>
